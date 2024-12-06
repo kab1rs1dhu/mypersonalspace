b017f54f4fdc85447face891e0563403f575184d
kabir sidhu <kabir_singh_sidhu@sfu.ca>
2024-12-05 21:06:06 -0800

    init commit

---
+++ src/main/java/com/mypersonalspace/mypersonalspace/MypersonalspaceApplication.java
@@ -0,0 +1,13 @@
+package com.mypersonalspace.mypersonalspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypersonalspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypersonalspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mypersonalspace/mypersonalspace/MypersonalspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypersonalspace.mypersonalspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypersonalspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
